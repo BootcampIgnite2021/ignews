bc4aa00a9169290b0b03895622640ad1f64e954f
Francisco júnior <franciscojn49@gmail.com>
2021-07-11 02:59:39 -0300

    feat: created page Home

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,7 @@
+import Styles from './styles.module.scss'
+
+export function SubscribeButton() {
+  return (
+    <button className={Styles.subscribeButton} type="button">Subscribe now</button>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import Styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news | Início</title>
+        <title>ig.news | Home</title>
       </Head>
+
+      <main className={Styles.contentConatiner}>
+        <section className={Styles.hero}>
+          <span> 👏 Hey, welcome</span>
+          <h1>News about
+            the <span>React</span> world</h1>
+          <p>Get acess to all the publications <br />
+            <span>for $9,90 month</span> </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Avatar" />
+      </main>
     </>
   )
 }
