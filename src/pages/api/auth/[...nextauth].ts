e34ab6d8548be17067b694e75eb3fe1235629ea0
Francisco júnior <franciscojn49@gmail.com>
2021-07-11 23:18:09 -0300

    feat: verify user created

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+
+import { faunadb } from "../../../services/faunadb";
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,31 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await faunadb.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
