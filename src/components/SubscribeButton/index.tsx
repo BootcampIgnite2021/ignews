98f2f747c67681b911b4e145d4b9db2b02aefee8
Francisco júnior <franciscojn49@gmail.com>
2021-07-12 22:55:56 -0300

    feat: add route subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { useSession, signIn } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
+
 import Styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,7 +9,27 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession()
+  
+  async function handleSubscribe() {
+    if(!session) {
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({sessionId})
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
-    <button className={Styles.subscribeButton} type="button">Subscribe now</button>
+    <button className={Styles.subscribeButton} type="button" onClick={handleSubscribe}>Subscribe now</button>
   )
 }
