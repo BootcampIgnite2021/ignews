d56e5bb0e18673b68d9f8f5824651fab8ad13e5a
Francisco Junior <franciscojn49@gmail.com>
2021-07-24 03:28:14 -0300

    feat: add event stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,8 +1,53 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
 
-export default (request: NextApiRequest, response: NextApiResponse) => {
-  console.log("evento recebido");
+async function buffer(readable: Readable) {
+  const chunks = [];
 
-  return response.status(200).json({ ok: true });
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const relevantEvents = new Set(["checkout.session.completed"]);
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === "POST") {
+    const buf = await buffer(request);
+    const secret = request.headers["stripe-signature"];
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf,
+        secret,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (error) {
+      return response.status(400).send(`webhook-error: ${error.message}`);
+    }
+
+    const { type } = event;
+
+    if (relevantEvents.has(type)) {
+      console.log("evento recebido", event);
+    }
+
+    return response.json({ received: true });
+  } else {
+    response.setHeader("Allow", "POST");
+    response.status(405).end("Method not allowed");
+  }
 };
