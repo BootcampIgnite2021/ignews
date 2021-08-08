24ea09dad0a98feadca5562d2fee7e88c36e60e7
Francisco Junior <franciscojn49@gmail.com>
2021-08-07 22:05:34 -0300

    feat: add listing posts in page posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,33 +2,35 @@ import Styles from './posts.module.scss';
 
 import { GetStaticProps } from 'next'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic';
 
-export default function Posts() {
+interface ItemsPosts {
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
+
+interface Posts {
+  posts: ItemsPosts[]
+}
+
+export default function Posts({ posts }: Posts) {
   return (
     <>
       <Head>ig.news | Posts</Head>
 
       <main className={Styles.container}>
         <div className={Styles.posts}>
-          <a href="">
-            <time>12 de março 2021</time>
-            <strong>safhjsdfklhsdfkjlshdfljksdhfkjsdhfklsjdfhksdl</strong>
-            <p>fsdfjsdfjsdflkjsdflksdfjlsdk</p>
-          </a>
-
-          <a href="">
-            <time>12 de março 2021</time>
-            <strong>safhjsdfklhsdfkjlshdfljksdhfkjsdhfklsjdfhksdl</strong>
-            <p>fsdfjsdfjsdflkjsdflksdfjlsdk</p>
-          </a>
-
-          <a href="">
-            <time>12 de março 2021</time>
-            <strong>safhjsdfklhsdfkjlshdfljksdhfkjsdhfklsjdfhksdl</strong>
-            <p>fsdfjsdfjsdflkjsdflksdfjlsdk</p>
-          </a>
+          {posts.map(post => (
+            <a href="" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -45,11 +47,24 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   )
 
-  console.log(JSON.stringify(response, null, 2));
+  // console.log(JSON.stringify(response, null, 2));
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: Intl.DateTimeFormat('pt-br', { 
+        day: '2-digit', 
+        month: 'long', 
+        year: 'numeric'
+       }).format(new Date(post.last_publication_date))
+    }
+  })
 
   return {
     props: {
-
+      posts
     }
   }
 }
